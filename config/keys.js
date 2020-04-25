d3bfd9ba9cb4869e56866eafb46336aaba785249
uriel zacarias <urielzacarias@gmail.com>
2020-04-25 02:55:35 -0700

    including keys for heroku version

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: '114202610601-5jdvrfbu2c3vjn4mi7gcq3s0c3a7g6d1.apps.googleusercontent.com',
+    googleClientSecret: 'RUA2hdkRyEJNXSchm80lzNlN',
+    mongoURI: 'mongodb+srv://2p9cHfewvSdse@W@cluster0-zivnw.mongodb.net/test?retryWrites=true&w=majority',
+//    coookieKey: '',
+}
+
+
+//api key
+// AIzaSyDdILEqoqc1qCuVe6tQ9TUA8EHl0sEXQvM
