9e36c9289706b97815f70665ebfb25297b1cad05
uriel zacarias <urielzacarias@gmail.com>
2020-04-27 01:33:45 -0700

    adding testing uri

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
+    mongoURI: 'mongodb+srv://avenskyprod:Starlight90@emaily-prod-r4akm.mongodb.net/test?retryWrites=true&w=majority',
     cookieKey: process.env.COOKIE_KEY,
 }
