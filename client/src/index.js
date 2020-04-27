9f677c27e4c676e707856fa0d6da3bbf9692e393
uriel zacarias <urielzacarias@gmail.com>
2020-04-27 15:48:23 -0700

    set up react stripe checkout

--- client/src/index.js
+++ client/src/index.js
@@ -22,6 +22,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY)
+console.log('Environment is', process.env.NODE_ENV)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
