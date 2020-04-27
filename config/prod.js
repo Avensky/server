560428bce3debda296f0a0570a69e5d3b716d8f7
uriel zacarias <urielzacarias@gmail.com>
2020-04-27 01:59:16 -0700

    updating env variable

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: 'mongodb+srv://avenskyprod:Starlight90@emaily-prod-r4akm.mongodb.net/test?retryWrites=true&w=majority',
+    mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
 }
