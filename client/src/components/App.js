d477b14d41ab19ba0fa6ca5099780c9b4d57b84e
uriel zacarias <urielzacarias@gmail.com>
2020-04-27 04:13:38 -0700

    app component converted to class component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
@@ -14,19 +14,25 @@ const SurveyNew = () => <h2>SurveyNew stuff</h2>;
 // import Dashboard from './Dashboard';
 // import SurveyNew from './surveys/SurveyNew';
 
-const App = () =>{
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="Container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
